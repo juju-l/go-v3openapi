f12162cba05fc65dea2a38f0e37dd89ba69a3324
root <lj@vipex.cc>
2026-05-20 23:24:24 +0800

    custom expand tag

--- v3/swagger-initializer.js
+++ v3/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "/openapi/v3",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -11,9 +11,23 @@ window.onload = function() {
       SwaggerUIStandalonePreset
     ],
     plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
+      SwaggerUIBundle.
+      plugins.
+      DownloadUrl
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    docExpansion: "none",
+    onComplete: function() {
+    ["Wx"].forEach(tag => {
+    const el = document.getElementById(
+    `operations-tag-${
+    encodeURIComponent(tag)
+    }`
+    );
+    if (el && !el.classList.contains('is-open') && true) { el?.click(); }
+    ///**
+    });
+    }
   });
 
   //</editor-fold>
---
+++ v3/tagsExpand.js
@@ -0,0 +1,12 @@
+
+function exp() {console.log("qq")
+	["Wx"].forEach(tag => {
+	const el = document.getElementById(`operations-tag-${encodeURIComponent(tag)}`);
+    if (el && !el.classList.contains('is-open') && true) {
+	el.click();
+	}
+	///**
+    });
+};
+
+
